237d8fab579a76d77d711d62ab103647d0d84d0d
nicholasscabral <nicholasscabral@gmail.com>
2021-09-09 01:38:45 -0300

    styling code-display view

--- server.js
+++ server.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const code = `Welcome to WasteBin!
+
+Use the commands in the top right corner
+to create a new file to share with others.`;
+
+  res.render("code-display", { code });
+});
+
 app.listen(3000, () => console.log("server running..."));
